8edce2f198d687df0c195ff50503b38ea105c6c2
Lapierre David <db.lapierre@gmail.com>
2021-12-10 20:50:08 +0100

    Ajout clock zone

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,21 @@ let currentPopup: any = undefined;
 const today = new Date();
 const time = today.getHours() + ":" + today.getMinutes();
 
-WA.room.onEnterZone('clock', () => {
-    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
-})
+// WA.room.onEnterZone('clock', () => {
+//     currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
+// })
 
-WA.room.onLeaveZone('clock', closePopUp)
+WA.room.onEnterLayer('clock').subscribe(()=>{
 
-function closePopUp(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
+    WA.chat.sendChatMessage(time, 'Mr Robot');
+    // currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
+});
+
+// WA.room.onLeaveZone('clock', closePopUp)
+// WA.room.onLeaveLayer('clock').subscribe(()=>{closePopUp()});
+// function closePopUp(){
+//     if (currentPopup !== undefined) {
+//         currentPopup.close();
+//         currentPopup = undefined;
+//     }
+// }
