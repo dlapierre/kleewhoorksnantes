0e9c5debff9dff45a6d51f7e5668933bb95fc2cd
dlapierre <db.lapierre@gmail.com>
2021-12-10 20:35:47 +0000

    Deploying to gh-pages from master @ 0272b7b77df1edea936141d2c50a874349e3937b 🚀

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
 
 import {bootstrapExtra} from "@workadventure/scripting-api-extra";
+import {Popup} from "@workadventure/iframe-api-typings/Api/iframe/Ui/Popup";
 
 // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
 bootstrapExtra().catch(e => console.error(e));
@@ -13,10 +14,30 @@ const time = today.getHours() + ":" + today.getMinutes();
 //     currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
 // })
 
+let imprimantePopup : Popup;
+
+WA.room.onEnterLayer('imprimanteZone').subscribe(()=>{
+
+    imprimantePopup = WA.ui.openPopup("imprimanteMessage", "Il n''y a plus de papier !", [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }]);
+});
+
+WA.room.onLeaveLayer("imprimanteZone").subscribe(() => {
+    if(imprimantePopup){
+        imprimantePopup.close();
+    }
+
+})
+
 WA.room.onEnterLayer('clock').subscribe(()=>{
 
     WA.chat.sendChatMessage(time, 'Mr Robot');
-    // currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
 });
 
 // WA.room.onLeaveZone('clock', closePopUp)
